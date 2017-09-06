e7510f230e68be893615965663222552fcedf221
PacersCyc <452720855@qq.com>
2017-09-07 01:26:35 +0800

    虚拟DOM

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-ReactDOM.render(
-  <h1>Hi, world!</h1>,
-  document.getElementById('root2')
-);
+
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+
+  ReactDOM.render(
+    element,
+    document.getElementById('root2')
+  );
+}
+
+setInterval(tick, 1000);
