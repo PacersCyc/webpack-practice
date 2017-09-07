cfe13e5137c2f6b95c7e809ada46c589dd80ca90
PacersCyc <452720855@qq.com>
2017-09-07 15:46:07 +0800

    测试leancloud功能

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import './App.css';
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import * as localStore from './localStore'
+import AV from 'leancloud-storage'
+
+var APP_ID = 'RzVW6hOCdSEuGpe83S6EBbCF-gzGzoHsz'
+var APP_KEY = 'Ea6jsOFmp5N3RgjWOqKu4e1E'
+AV.init({
+  appId:APP_ID,
+  appKey:APP_KEY
+})
+
+var TestObject = AV.Object.extend('TestObject');
+var testObject = new TestObject();
+testObject.save({
+  words: 'Hello World!'
+}).then(function(object) {
+  alert('LeanCloud Rocks!');
+})
 
 class App extends Component {
   constructor(props){
