6e7ff0e9fa44b59edcdcc9c387afdc9898fc3f42
PacersCyc <452720855@qq.com>
2017-09-07 08:42:45 +0800

    lifecycle

--- src/welcome.js
+++ src/welcome.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
 class Welcome extends React.Component { 
-  render() {
-    //return <h1>Hello, Component</h1>;
-    return <h1>Hello,{this.props.name}</h1>
-  }
+	constructor(props){
+		super(props)
+		this.state = {
+			date: new Date()
+		}
+		setInterval(()=>{
+			this.setState({
+				date: new Date()
+			})	
+		},1000)
+		console.log('初始化states和props')
+	}
+
+	componentWillMount(){
+		console.log('即将进行render')
+	}
+
+	render(){
+		console.log('正在render')
+		return (
+			<div>
+				<h1>Hello, {this.props.name}</h1>
+				<h2>{this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		)
+	}
+
+	componentDidMount(){
+		console.log('已经挂载到页面里了')
+	}
 }
 
 export default Welcome
